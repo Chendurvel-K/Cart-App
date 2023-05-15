204c50bf9b6aef5f99b041276e87364ced956556
Chendurvel-K <chendurvel.kumaresan@ideas2it.com>
2023-05-15 14:46:23 +0530

    EBook app completed with firebase

--- src/screens/PaymentScreen.js
+++ src/screens/PaymentScreen.js
@@ -1,50 +1,54 @@
-import { FlatList, StyleSheet, Text, View, ScrollView } from "react-native";
+import {
+  FlatList,
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  SafeAreaView,
+  TouchableOpacity,
+} from "react-native";
 import React, { useState } from "react";
 import useCart from "../navigation/useCart";
+import { CheckBox } from "react-native-elements";
 
 function Separator() {
-  return (
-    <View style={{ borderBottomWidth: 1, borderBottomColor: "#a9a9a9" }} />
-  );
+  return <View style={{ borderBottomWidth: 1, borderBottomColor: "#1222" }} />;
 }
 
 const PaymentScreen = () => {
   const { cart } = useCart();
+  const [checked, setChecked] = useState(false);
   const [isChecked, setIsChecked] = useState(false);
+  const [selected, setSelected] = useState(false);
+  const [isSelected, setIsSelected] = useState(false);
+
   console.log("cart", cart);
+
+  const handleSubtotal = () => {
+    console.log("Item lenght", cart.length);
+    let SubTotal = 0;
+    for (let i = 0; i < cart.length; i++) {
+      SubTotal += cart[i].price * cart[i].quantity;
+    }
+    return SubTotal;
+  };
+
+  const handlePayment = () => {
+    if (checked || isChecked || selected || isSelected) {
+      alert("Payment Success");
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      {/* <View style={styles.titleContainer}>
-        <Text
-          style={[
-            styles.titleText,
-            { backgroundColor: null, paddingHorizontal: "20%" },
-          ]}
-        >
-          Name
-        </Text>
-        <Text
-          style={[
-            styles.titleText,
-            { backgroundColor: null, paddingHorizontal: "6%" },
-          ]}
-        >
-          Qty
-        </Text>
-        <Text
-          style={[
-            styles.titleText,
-            { backgroundColor: null, paddingHorizontal: "5%" },
-          ]}
-        >
-          Price
-        </Text>
-      </View> */}
-      <View style={styles.listContainer}>
-        <FlatList
-          data={cart}
-          renderItem={({ item }) => (
-            <View style={styles.itemContainer}>
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+        <View style={styles.listContainer}>
+          {/* <FlatList
+            data={cart}
+            ItemSeparatorComponent={Separator}
+            renderItem={({ item }) => ( */}
+          {cart.map((item) => (
+            <View key={item.id} style={styles.itemContainer}>
               <View
                 style={{
                   flex: 10,
@@ -58,8 +62,8 @@ const PaymentScreen = () => {
                   numberOfLines={2}
                   style={{
                     // backgroundColor: "red",
-                    padding: 20,
-                    marginHorizontal: 0,
+                    // marginHorizontal: 0,
+                    padding: 15,
                   }}
                 >
                   {item.name}
@@ -74,10 +78,12 @@ const PaymentScreen = () => {
                 }}
               >
                 <Text
-                  style={{
-                    // backgroundColor: "blue",
-                    padding: 10,
-                  }}
+                  style={
+                    {
+                      // backgroundColor: "blue",
+                      // padding: 10,
+                    }
+                  }
                 >
                   {item.quantity}
                 </Text>
@@ -91,30 +97,97 @@ const PaymentScreen = () => {
                 }}
               >
                 <Text
-                  style={{
-                    // backgroundColor: "yellow",
-                    padding: 10,
-                  }}
+                  style={
+                    {
+                      // backgroundColor: "yellow",
+                      // padding: 10,
+                    }
+                  }
                 >
                   {item.price * item.quantity}
                 </Text>
               </View>
             </View>
+          ))}
+        </View>
+        <View style={[styles.listContainer, { padding: 20 }]}>
+          <CheckBox
+            title={"Debit/Credit Card"}
+            checked={checked}
+            onPress={() => setChecked(!checked)}
+            checkedColor="#FF7F50"
+          />
+          <CheckBox
+            title={"UPI Payment"}
+            checked={isChecked}
+            onPress={() => setIsChecked(!isChecked)}
+            checkedColor="#FF7F50"
+          />
+          <CheckBox
+            title={"Wallet Pay"}
+            checked={selected}
+            onPress={() => setSelected(!selected)}
+            checkedColor="#FF7F50"
+          />
+          <CheckBox
+            title={"Cash on Delivery"}
+            checked={isSelected}
+            onPress={() => setIsSelected(!isSelected)}
+            checkedColor="#FF7F50"
+          />
+        </View>
+        <View
+          style={{
+            flex: 1,
+            flexDirection: "row",
+            borderRadius: 12,
+            backgroundColor: "#FFF",
+            // width: "95%",
+            // height: "70%",
+            margin: 20,
+            padding: 10,
+            justifyContent: "space-around",
+            alignItems: "center",
+          }}
+        >
+          <Text
+            style={{
+              color: "#000",
+              fontWeight: "bold",
+              fontSize: 20,
+              marginLeft: 8,
+            }}
+          >
+            SubTotal $ {handleSubtotal()}{" "}
+          </Text>
+          {cart.length !== 0 ? (
+            <TouchableOpacity
+              onPress={() => handlePayment()}
+              style={{
+                borderRadius: 12,
+                backgroundColor: "#FF7F50",
+                alignItems: "center",
+              }}
+            >
+              <Text
+                style={{
+                  color: "#FFF",
+                  fontWeight: "bold",
+                  fontSize: 20,
+                  marginLeft: 0,
+                  padding: 10,
+                  paddingHorizontal: 40,
+                }}
+              >
+                Pay
+              </Text>
+            </TouchableOpacity>
+          ) : (
+            <TouchableOpacity disabled={true} />
           )}
-        />
-      </View>
-      <View style={{}}>
-        <View>
-          <Text>HIIIIII</Text>
         </View>
-        <View>
-          <Text>HEllooo</Text>
-        </View>
-        <View>
-          <Text>Welcome</Text>
-        </View>
-      </View>
-    </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
@@ -122,8 +195,9 @@ export default PaymentScreen;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 0,
+    flex: 1,
     // backgroundColor: "red",
+    justifyContent: "space-between",
   },
   titleContainer: {
     flex: 0,
@@ -150,3 +224,87 @@ const styles = StyleSheet.create({
     padding: 10,
   },
 });
+
+// import { ScrollView, StyleSheet, Text, View } from "react-native";
+// import React, { useState } from "react";
+// import useCart from "../navigation/useCart";
+// import { CheckBox } from "react-native-elements";
+
+// function Separator() {
+//   return (
+//     <View style={{ borderBottomWidth: 1, borderBottomColor: "#a9a9a9" }} />
+//   );
+// }
+
+// const PaymentScreen = () => {
+//   const { cart } = useCart();
+//   const [isChecked, setIsChecked] = useState(false);
+//   console.log("cart", cart);
+//   return (
+//     <View style={styles.container}>
+//       <ScrollView>
+//         <View style={styles.listContainer}>
+//           {cart.map((item) => (
+//             <View key={item.id} style={styles.itemContainer}>
+//               <View
+//                 style={{
+//                   flex: 10,
+//                   borderRadius: 12,
+//                   justifyContent: "center",
+//                   padding: 15,
+//                 }}
+//               >
+//                 <Text numberOfLines={2}>{item.name}</Text>
+//               </View>
+//               <View
+//                 style={{
+//                   flex: 2,
+//                   alignItems: "center",
+//                   justifyContent: "center",
+//                 }}
+//               >
+//                 <Text>{item.quantity}</Text>
+//               </View>
+//               <View
+//                 style={{
+//                   flex: 3,
+//                   justifyContent: "center",
+//                   alignItems: "center",
+//                 }}
+//               >
+//                 <Text>{item.price * item.quantity}</Text>
+//               </View>
+//             </View>
+//           ))}
+//         </View>
+//         <View style={[styles.listContainer, { padding: 20 }]}>
+//           <CheckBox title={"Debit/Credit Card"} />
+//           <CheckBox title={"UPI Payment"} />
+//           <CheckBox title={"Wallet Pay"} />
+//           <CheckBox title={"Cash on Delivery"} />
+//         </View>
+//       </ScrollView>
+//     </View>
+//   );
+// };
+
+// export default PaymentScreen;
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: "space-between",
+//   },
+//   listContainer: {
+//     backgroundColor: "#FFF",
+//     borderRadius: 12,
+//     margin: 20,
+//   },
+//   itemContainer: {
+//     flexDirection: "row",
+//     justifyContent: "space-around",
+//     alignItems: "stretch",
+//     margin: 0,
+//     padding: 10,
+//   },
+// });
